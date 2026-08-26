84151c19d7f867820aeba6dff1dbb9e4fc73b02d
anixlll <aniiilll3326@gmail.com>
2026-08-26 14:12:05 +0200

    Add sample books

--- Book-Library-API/Book-Library-API/Services/BookService.cs
+++ Book-Library-API/Book-Library-API/Services/BookService.cs
@@ -6,6 +6,27 @@ public class BookService : IBookService
 {
     private readonly List<Book> _books = new();
 
+    public BookService()
+    {
+        _books.Add(new Book
+        {
+            Id = 1,
+            Title = "The Last Days of Socrates",
+            Author = "Plato",
+            Pages = 256,
+            Year = 1954
+        });
+        
+        _books.Add(new Book
+        {
+            Id = 2,
+            Title = "Metamorphosis",
+            Author = "Franz Kafka",
+            Pages = 70,
+            Year = 1915
+        });
+    }
+    
     public List<Book> GetBooks()
     {
         return _books.ToList(); // Kopie der Liste
